fd5aa967a2a2c4ce9156887b8380d23a009f795e
Snezhana <snezhana.yarovaya@gmail.com>
2018-08-23 01:02:30 +0300

    add new img

--- gulpfile.js
+++ gulpfile.js
@@ -213,10 +213,6 @@ gulp.task('copy', function () {
         .pipe(gulp.dest(bases.dist + 'images/'))
         .pipe(reload({stream: true}));
 
-    gulp.src(bases.app + 'fonts/**/*.*')
-        .pipe(size({gzip: true, showFiles: true}))
-        .pipe(gulp.dest(bases.dist + 'fonts/'))
-        .pipe(reload({stream: true}));
 
     gulp.src(bases.app + 'video/**/*.*')
         .pipe(size({gzip: true, showFiles: true}))
