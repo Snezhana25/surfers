438a0440c613f5ca7486a4757e43449cec2952b4
Snezhana <snezhana.yarovaya@gmail.com>
2018-09-03 12:32:20 +0300

    control commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,6 @@
-// let oTab = document.getElementById('photo-tab');
-// let oLis = oTab.getElementsByClassName('tabs-photo');
-// let oDivs = oTab.getElementsByClassName('.photo');
-//
-// for (let i = 0; i < oLis.length; i++) {
-//     let oLi = oLis[i];
-//     oLi.n = i;
-//     oLi.onclick = function () {
-//         for (var j = 0; j < oLis.length ; j++) {
-//             oLis[j].classList = '';
-//             oDivs[j].className = '';
-//         }
-//         console.log(this);
-//         this.className = "active-photo";
-//         oDivs[this.n].className = "active-photo";
-//     };
-// }
-
 
 const tabs = document.querySelectorAll('.tabs-photo');
-const selectedImg = document.getElementById('selected-img');
+const selectedImg = document.querySelector('.selectedImg');
 
 if (tabs.length > 0 && selectedImg ) {
     tabs.forEach((tab) => {
@@ -31,6 +13,69 @@ if (tabs.length > 0 && selectedImg ) {
         })
     });
 }
+
+
+import {tns} from 'tiny-slider/src/tiny-slider';
+
+var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    nav: false,
+    controlsText:['', ''],
+    slideBy: 'page',
+    cloneCount: false,
+    loop: false
+});
+
+
+const videoimg = document.querySelector('.btn-img-video')
+
+videoimg.addEventListener("click", function(){
+    console.log("hi");
+    videoimg.style.display = "none";
+})
+
+
+const linkNav = document.querySelectorAll('a[href^="#"]');
+
+    linkNav.forEach(function(item){
+        item.addEventListener('click', function(e){
+            console.log(item.getAttribute('data-anchor'));
+            let anchor = document.getElementById(item.getAttribute('data-anchor')).getBoundingClientRect().top;
+                // console.log(document.querySelector(item.getAttribute('href')).getBoundingClientRect());
+            let scroller = setInterval(function() {
+                window.scrollTo({
+                    top: anchor,
+                    behavior: "smooth"
+                });
+                clearInterval(scroller);
+            },300);
+        });
+
+    });
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.magic-nav');
+const topNav = document.querySelector('.top-nav');
+const link = topNav.querySelectorAll('li');
+    menuBtn.addEventListener('click', function(e){
+        if(nav.classList.contains("open")){
+            nav.classList.remove("open");
+        }else{
+            nav.classList.add("open");
+        }
+
+        for (let i = 0; i < link.length; i++) {
+            link[i].addEventListener('click', function () {
+                console.log(link);
+                nav.classList.remove("open");
+            });
+        }
+
+    });
+
+
 
 
 
