14fb0642f5925ea530ebef89622e6a0fe3f8bf3e
Snezhana <snezhana.yarovaya@gmail.com>
2018-09-09 14:16:19 +0300

    change slider and menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,79 +1,88 @@
+(function () {
+    "use strict";
+    const tabs = document.querySelectorAll('.tabs-photo');
+    const selectedImg = document.querySelector('.selectedImg');
+    const sdImg = document.querySelector('.sdImg');
 
-const tabs = document.querySelectorAll('.tabs-photo');
-const selectedImg = document.querySelector('.selectedImg');
+    if (tabs.length > 0 && selectedImg || tabs.length > 0 && sdImg) {
+        tabs.forEach((tab) => {
+            tab.addEventListener('click', (e) => {
+                e.stopPropagation();
+                if (tab.classList.contains('tabs-slide1')) {
+                    selectedImg.src = tab.childNodes[0].src;
+                } else {
+                    tab.classList.contains('tabs-slide2');
+                    sdImg.src = tab.childNodes[0].src;
+                }
+                tabs.forEach(elem => {
+                    elem.classList.remove('activeTab');
+                });
+                tab.classList.add('activeTab');
+            }, true)
+        });
 
-if (tabs.length > 0 && selectedImg ) {
-    tabs.forEach((tab) => {
-        tab.addEventListener('click', (e) => {
-            selectedImg.src = e.target['src'];
-            tabs.forEach(elem => {
-                elem.classList.remove('activeTab');
-            });
-            tab.classList.add('activeTab');
-        })
+    }
+
+    const videoimg = document.querySelector('.btn-img-video');
+    const iframe = document.querySelector('.iframe');
+
+    videoimg.addEventListener("click", function (event) {
+        console.log(event.target);
+        videoimg.style.display = "none";
+        const iframeEl = document.createElement('iframe');
+        iframeEl.src = 'https://www.youtube.com/embed/DkeiKbqa02g?;autoplay=1&;';
+        iframe.appendChild(iframeEl);
     });
-}
 
+    const linkNav = document.querySelectorAll('a[href^="#"]');
 
-import {tns} from 'tiny-slider/src/tiny-slider';
-
-var slider = tns({
-    container: '.my-slider',
-    items: 1,
-    nav: false,
-    controlsText:['', ''],
-    slideBy: 'page',
-    cloneCount: false,
-    loop: false
-});
-
-
-const videoimg = document.querySelector('.btn-img-video')
-
-videoimg.addEventListener("click", function(){
-    console.log("hi");
-    videoimg.style.display = "none";
-})
-
-
-const linkNav = document.querySelectorAll('a[href^="#"]');
-
-    linkNav.forEach(function(item){
-        item.addEventListener('click', function(e){
-            console.log(item.getAttribute('data-anchor'));
+    linkNav.forEach(function (item) {
+        item.addEventListener('click', function () {
             let anchor = document.getElementById(item.getAttribute('data-anchor')).getBoundingClientRect().top;
-                // console.log(document.querySelector(item.getAttribute('href')).getBoundingClientRect());
-            let scroller = setInterval(function() {
+            let scroller = setInterval(function () {
                 window.scrollTo({
                     top: anchor,
                     behavior: "smooth"
                 });
                 clearInterval(scroller);
-            },300);
+            }, 300);
         });
-
     });
 
 
-const menuBtn = document.querySelector('.menu-btn');
-const nav = document.querySelector('.magic-nav');
-const topNav = document.querySelector('.top-nav');
-const link = topNav.querySelectorAll('li');
-    menuBtn.addEventListener('click', function(e){
-        if(nav.classList.contains("open")){
-            nav.classList.remove("open");
-        }else{
-            nav.classList.add("open");
-        }
-
+    const menuBtn = document.querySelector('.menu-btn');
+    const topNav = document.querySelector('.top-nav');
+    const link = topNav.querySelectorAll('li');
+    menuBtn.addEventListener('click', function () {
+        menuBtn.classList.toggle('clicked');
+        topNav.classList.toggle('show');
         for (let i = 0; i < link.length; i++) {
             link[i].addEventListener('click', function () {
-                console.log(link);
-                nav.classList.remove("open");
+                topNav.classList.remove("show");
+                menuBtn.classList.remove('clicked');
+
             });
         }
 
     });
+
+
+    const scrollnav = document.querySelector('.top-nav');
+    let menu = true;
+    // const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollnav.classList.contains('show')) {
+        console.log('true');
+
+    } else {
+        console.log('false');
+
+
+    }
+
+
+})();
+
 
 
 
